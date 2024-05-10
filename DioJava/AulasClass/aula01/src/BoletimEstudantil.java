fea08f9c3ff62307d808b56bea33a604bd80b79c
eliasdevmin <eliasaraujo1222@outlook.com>
2024-05-09 23:10:41 -0300

    novo

---
+++ DioJava/AulasClass/aula01/src/BoletimEstudantil.java
@@ -0,0 +1,15 @@
+public class BoletimEstudantil {
+   
+   
+    public static void main(String[] args) {
+        
+        int mediaFinal = 6;
+        
+        if(mediaFinal<6)
+            System.out.println("reprovado");
+        else if(mediaFinal==6)
+            System.out.println("Prova minerva");
+        else
+            System.out.println("Aprovado");;
+    }
+}
